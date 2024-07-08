f173bc68ae845e0119203c3df01df3699c9ac463
Evan Brothers <41376890+ekbrothers@users.noreply.github.com>
2024-07-08 14:28:32 -0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,10 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const gameRoutes = require('./routes/game');
 
+app.use(cors());
+app.use('/api', gameRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
