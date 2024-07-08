de04acbf84b769ecfe6ad278c2b747c2bbcf1f18
Evan Brothers <41376890+ekbrothers@users.noreply.github.com>
2024-07-08 14:31:33 -0400

    Update StadiumMap.js

--- src/components/StadiumMap.js
+++ src/components/StadiumMap.js
@@ -1,1 +1,34 @@
 
+import React from 'react';
+
+const StadiumMap = ({ stadium }) => {
+  const { sections } = stadium.seatingChart;
+
+  return (
+    <div>
+      <h2>{stadium.stadium} Seating Chart</h2>
+      <div style={{ display: 'flex' }}>
+        {sections.map(section => (
+          <div
+            key={section.id}
+            style={{
+              padding: '10px',
+              margin: '5px',
+              border: '1px solid black',
+              backgroundColor:
+                section.sun === 'full'
+                  ? 'yellow'
+                  : section.sun === 'partial'
+                  ? 'orange'
+                  : 'lightgrey',
+            }}
+          >
+            {section.id} - {section.sun}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default StadiumMap;
