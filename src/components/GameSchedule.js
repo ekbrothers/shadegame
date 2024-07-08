62dcff85495952a1653395b3313026706dcfc6b1
Evan Brothers <41376890+ekbrothers@users.noreply.github.com>
2024-07-08 14:31:20 -0400

    Update GameSchedule.js

--- src/components/GameSchedule.js
+++ src/components/GameSchedule.js
@@ -1,1 +1,14 @@
+import React from 'react';
+
+const GameSchedule = ({ schedule }) => {
+  return (
+    <div>
+      <h2>Game Schedule</h2>
+      <p>{schedule.game.date} - {schedule.game.time}</p>
+      <p>{schedule.game.teams}</p>
+    </div>
+  );
+};
+
+export default GameSchedule;
 
