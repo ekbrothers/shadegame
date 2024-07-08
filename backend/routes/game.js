29a721d9f0894b56e6178e568f7514a213d40c56
Evan Brothers <41376890+ekbrothers@users.noreply.github.com>
2024-07-08 14:28:48 -0400

    Update game.js

--- backend/routes/game.js
+++ backend/routes/game.js
@@ -1,1 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const schedule = require('../data/schedule.json');
+const stadium = require('../data/stadium.json');
 
+router.get('/schedule', (req, res) => {
+  res.json(schedule);
+});
+
+router.get('/stadium', (req, res) => {
+  res.json(stadium);
+});
+
+module.exports = router;
